0a0503379d10f2e469fa8a82d297285912941f7c
Daniel} <daniel.arayapaniagua@hotmail.com>
2022-06-21 21:59:18 -0600

    Crypto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'crypto';
+ 
+  selectedCurrency :string ="INR";
+  
+  constructor() { }
+
+  sendCurrency(event: string) {
+    console.log(event);
+
+    }
+
+
+
 }
